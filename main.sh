cfb2c986a6c49c54e212056303191ad22f3e35f5
mariusjabami <rootless137@gmail.com>
2024-09-22 20:10:56 +0000

    Atualizado V0.1

--- main.sh
+++ main.sh
@@ -5,12 +5,18 @@ name=$1
 while IFS= read -r url; do
       status=$(curl -s -o dev/null -w "%{http_code}" "$url/$1/")
 #      echo "${status}"
-     if [ $status -eq 200 ]; then
+     if [ $status -ge 200 ] && [ $status -le 226  ]; then
 
 
-         echo -e "\033[32m${url}$1/\033[0m";
+         echo -e "\033[32m${url}$1/\033[0m ${status}";
+    
+     fi
 
+     if [ $status -ge 300 ] && [ $status -le 308 ]; then
+
+        echo -e "\033[35m${url}$1/\033[0m ${status}";
 
      fi
+
 
 done < urls.txt
